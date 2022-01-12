8698d6d036a965b7978382f2b75c32ece224d36b
Najsiswjek <58403387+afedora@users.noreply.github.com>
2022-01-12 20:58:45 +0800

    Add files via upload

--- coba2.py
+++ coba2.py
@@ -13,9 +13,9 @@ O='\033[33m'
 E='\033[31m'
 
 session=requests.Session()
-home_url = 'https://mobile.facebook.com'
+home_url = 'https://mbasic.facebook.com'
 header = {
-	'Host':'mobile.facebook.com',
+	'Host':'mbasic.facebook.com',
 		'cache-control':'max-age=0',
 	'upgrade-insecure-requests':'1',
 		'user-agent':'Mozilla/5.0 (Linux; Android 11; SM-A325F Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36',
@@ -35,7 +35,7 @@ def login () :
 	header.update({"cookie" : str(cok) })
 	try :
 		f=session.get(home_url, headers=header).text
-		if 'mobile_logout_button' in str(f):
+		if 'mbasic_logout_button' in str(f):
 			save=open('coki.log', 'w')
 			save.write(str(cok))
 			save.close()
@@ -75,7 +75,7 @@ class Report():
 	def menu(self):
 		try :
 			orein=Scraping(webGet(home_url+"/profile.php?"), "html.parser")
-			if 'mobile_logout_button' in str(orein):
+			if 'mbasic_logout_button' in str(orein):
 				name=str(orein.find('title').text)
 			else :
 				exit('{}({}!{}){} Cookies mati'.format(W,R,W,Y))
